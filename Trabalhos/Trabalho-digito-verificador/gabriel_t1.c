9dac0d723fe17cfae6ecb1e30dafc80957b88e3d
gb1456v <gabrielvieira794@gmail.com>
2025-03-29 13:45:26 -0300

    adicionado pdf das aulas e exercicios delas

---
+++ Trabalhos/Trabalho-digito-verificador/gabriel_t1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    //declaração de variáveis
+    int registro_sem_digitos;
+    
+    
+    printf("Digite os dígitos do seu Registro para que o programa calcule os digitos vericadores dele");
+    scanf("%d", &registro_sem_digitos);
+
+    
+
+    
+}
