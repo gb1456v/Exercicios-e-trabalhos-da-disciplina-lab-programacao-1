938371fdfa5c61525baa3e4750ea3f3a8f2a681d
gb1456v <gabrielvieira794@gmail.com>
2025-03-29 16:27:11 -0300

    revisão dos comentários do código

--- Trabalhos/Trabalho-digito-verificador/gabriel_t1.c
+++ Trabalhos/Trabalho-digito-verificador/gabriel_t1.c
@@ -6,8 +6,8 @@ int main() {
     int dig_verificador1, dig_verificador2; // digitos verificadores
     int soma_S1, soma_S2, div_1,  div_2; // somas e divisões
     
-    //mostra uma mensagem informando pra digitar os dígitos do Registro separadamente
-    printf("Digite os dígitos do seu Registro conforme forem solicitados para que o programa calcule os digitos vericadores dele\n");
+    //mostra uma mensagem que pede os  dígitos do Registro separadamente
+    printf("Digite os dígitos do seu Registro Individual conforme forem solicitados para que o programa calcule os digitos vericadores dele\n");
     //pede cada um dos dígitos
     printf("Digite o 1o digito:\n");
     scanf("%d",&dig1);
@@ -29,8 +29,7 @@ int main() {
     scanf("%d",&dig9);
 
     // calculo do primeiro dígito verificador
-    soma_S1 = (dig1 * 1 + dig2 * 2 + dig3 * 3 + dig4 * 4 + dig5 * 5 + dig6 * 6 + dig7 * 7 + dig8 * 8 + dig9 * 9); 
-    // obtém a soma S
+    soma_S1 = (dig1 * 1 + dig2 * 2 + dig3 * 3 + dig4 * 4 + dig5 * 5 + dig6 * 6 + dig7 * 7 + dig8 * 8 + dig9 * 9); // obtém a soma S do primeiro dígito vericador
 
     div_1 = soma_S1 % 11; // divide o resultado da soma por 11 e pega o resto
    
@@ -41,11 +40,11 @@ int main() {
     } else {
         dig_verificador1 = div_1;
     }
-    //fim primeiro dígito
+    //fim calculo do primeiro dígito vericador
 
-    //segundo dígito vericador
+    //calculo do segundo dígito vericador
     
-    soma_S2 = (dig1 * 0 + dig2 * 1 + dig3 * 2 + dig4 * 3 + dig5 * 4 + dig6 * 5 + dig7 * 6 + dig8 * 7 + dig9 * 8 + dig_verificador1 * 9); // obtém o segundo dígito verificador
+    soma_S2 = (dig1 * 0 + dig2 * 1 + dig3 * 2 + dig4 * 3 + dig5 * 4 + dig6 * 5 + dig7 * 6 + dig8 * 7 + dig9 * 8 + dig_verificador1 * 9); // obtém a soma S do segundo dígito vericador
     div_2 = soma_S2 % 11; //obtém m resto da divisão por 11
     
     if (div_2 == 10) {
@@ -53,8 +52,8 @@ int main() {
     } else {
         dig_verificador2 = div_2;
     }
-    // fim segundo dígito
+    // fim  calculo segundo digitosegundo dígito
 
-    // mostra o Registro com os dígitos vericadores
+    // mostra o Registro Individual com os dígitos vericadores
     printf("O seu Registro Individual e: \n %d%d%d%d%d%d%d%d%d/%d%d", dig1, dig2, dig3, dig4, dig5,dig6,dig7,dig8,dig9, dig_verificador1, dig_verificador2);
 }
