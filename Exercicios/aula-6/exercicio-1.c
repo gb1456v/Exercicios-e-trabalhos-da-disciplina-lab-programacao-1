e6216df38730b3f17605d63a04ce86243fe9f69b
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-04-01 15:34:52 -0300

    parte do exercicio 1 feito

---
+++ Exercicios/aula-6/exercicio-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int i, valores;
+float a,b,c;
+
+printf("Digite os valores para i, a, b e c respectivamente");
+scanf("%d%f%f%f",&i,&a,&b,&c);
+switch(i) {
+    case 1: if(( a > b) && (b > c) && (c>a)) {
+        printf(" %f, %f, %f", a,b, c);
+    } else if ((a<b) && (b>c)) {
+        printf(" %f, %f, %f", b,a, b);
+    } else if (
+
+    )
+}
