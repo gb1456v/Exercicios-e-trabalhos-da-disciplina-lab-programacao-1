31f5fbfe1037bbf4baf15952b162e4dc53bbef93
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-04-09 03:11:41 +0000

    prototipo exercicio-1 aula-7

---
+++ Exercicios/aula-7/exercicio-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int intervalo_inicio,intervalo_fim;
+    int i=0, soma=0,cont=0;
+    printf("Digite o valor do ínicio do intervalo");
+    scanf("%d", &intervalo_inicio);
+    printf("Digite o valor do fim do intervalo");
+    scanf("%d", &intervalo_fim);
+    for(i=intervalo_inicio; i<intervalo_fim; i++) {
+        cont++; // 1
+        if(                                                                                                              % 2 == 1) {
+
+            soma = soma + cont;
+        }
+    printf("A soma dos ")
+    }
+
+}
