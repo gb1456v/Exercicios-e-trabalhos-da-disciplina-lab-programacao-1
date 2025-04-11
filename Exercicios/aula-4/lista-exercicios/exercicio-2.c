3ce1cc747678f2893fcafca0880ad5544d190dd7
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-04-11 12:32:46 +0000

    finalizado exercicio-2-aula-4

--- Exercicios/aula-4/lista-exercicios/exercicio-2.c
+++ Exercicios/aula-4/lista-exercicios/exercicio-2.c
@@ -3,8 +3,12 @@
 int main() {
     int a= 5;
     int b= 2;
+    int c;
     float d;
-    d =(float)a/(float)b;
+    c = (float)a/(float)b;
+    d= (float)c;
+
+
     printf("O valor de d e: %.2f", d);
     
 
