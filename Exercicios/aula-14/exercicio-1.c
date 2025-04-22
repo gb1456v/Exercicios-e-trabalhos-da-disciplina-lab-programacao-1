05c92cf0410accf08dbc5473bfaa300ca48f18ed
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-04-22 19:15:43 +0000

    exercicio-1 iniciado

--- Exercicios/aula-14/exercicio-1.c
+++ Exercicios/aula-14/exercicio-1.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
-
-int vetor_10_valores(int vetor[10]) {
-    int i;
+int main() {
+    int vetor[10], cont = 0, v, i;
+    //pede 1 numero para cada posicao do vetor
     for(i = 0; i < 10; i++) {
         scanf("%d", &vetor[i]);
     }
-    return vetor[i];
-}
-int main() {
-    int vetor[10], cont = 0;
-    vetor_10_valores(vetor[10]);
+    //pega um valor digitado pelo usuário e verifica se está no vetor
     printf("Digite um valor:\n");
     scanf("%d", &v);
     for(i=0; i<10; i++) {
         if(v == vetor[i]) {
         cont++;
-        }
-        
+        }    
     }
+    printf("O numero foi encontrado %d vezes dentro do vetor", cont);
 }
