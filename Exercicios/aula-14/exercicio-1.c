f0e466361759841851e753033a6bbb2a5d9dc612
gb1456 <gabrielvieira794@gmail.com>
2025-04-28 21:33:19 -0300

    trabalho iniciado

--- Exercicios/aula-14/exercicio-1.c
+++ Exercicios/aula-14/exercicio-1.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
-int main() {
-    int vetor[10], cont = 0, v, i;
+int preenche_vetor(int vetor[10]) {
+    int i=1; 
+    int cont_numero=1;
     //pede 1 numero para cada posicao do vetor
     for(i = 0; i < 10; i++) {
+        printf("Digite o valor %d: \n", cont_numero);
         scanf("%d", &vetor[i]);
+        cont_numero++;
     }
+}
+void procura_numero(int vetor[10]) {
+    int cont = 0;
+    int v;
+    int i = 0;
     //pega um valor digitado pelo usuário e verifica se está no vetor
-    printf("Digite um valor:\n");
+    printf("Digite o numero que voce quer procurar dentro do vetor:\n");
     scanf("%d", &v);
     for(i=0; i<10; i++) {
         if(v == vetor[i]) {
@@ -14,4 +22,10 @@ int main() {
         }    
     }
     printf("O numero foi encontrado %d vezes dentro do vetor", cont);
+}
+int main() {
+    int vetor[10];
+    int cont = 0, v, i = 0;
+    preenche_vetor(vetor[10]);
+    procura_numero(vetor[10]);
 }
